1f9ed9cf2281112beba8f136eede267cd7941977
MDMIMACat <romanmdima@gmail.com>
2023-12-19 10:27:52 +0000

    Rock, Paper scissors game, that plays for 5 turns in the console

--- javascript/script.js
+++ javascript/script.js
@@ -14,36 +14,52 @@ function getUserChoice() {
     return userInput.toLowerCase();
 }
 
-// Calling the getUserChoice function
-const playerSelection = getUserChoice();
-// Calling the getComputerChoice function
-const computerSelection = getComputerChoice();
 
 // playRound function that shows the potential outcomes from rock, paper, scissors game
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'rock') {
-        console.log("It's a tie");
+        return ("It's a tie");
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log("You lose! Paper beats rock");
+        return ("You lose! Paper beats rock");
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log("You win! Rock beats scissors");
+        return ("You win! Rock beats scissors");
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log("You win! Paper beats rock");
+        return ("You win! Paper beats rock");
     } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        console.log("It's a tie");
+        return ("It's a tie");
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log("You lose! Scissors beats paper");
+        return ("You lose! Scissors beats paper");
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log("You lose! Rock beats scissors");
+        return ("You lose! Rock beats scissors");
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log("You win! Scissors beats paper");
+        return ("You win! Scissors beats paper");
     } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-        console.log("It's a tie");
+        return ("It's a tie");
     } else {
-        console.log('Invalid entry, Try Again')
+        return ('Invalid entry, Try Again')
     }
 }
-
 // Calling the playRound, to play a singleround of rock-paper-scissors
-playRound(playerSelection, computerSelection);
+
+function game () {
+    const turns = 5;
+    const resultList = [];
+    for (let i = 0; i < turns; i++)
+    {
+        // Calling the getUserChoice function
+        const playerSelection = getUserChoice();
+        // Calling the getComputerChoice function
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection)
+        console.log(result)
+        resultList.push(result)
+    }
+    
+
+    console.log(resultList)
+
+}
+
+game();
+   
